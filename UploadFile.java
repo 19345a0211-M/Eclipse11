5057f5fe524cb174d6d15eb34f95e71f52ad6fa8
19345a0211-M <137747459+19345a0211-M@users.noreply.github.com>
2023-07-11 16:07:10 +0530

    Source code

---
+++ UploadFile.java
@@ -0,0 +1,25 @@
+package Demo;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UploadFile {
+	public static void main(String[] args) throws IOException, InterruptedException { 
+		
+	// 1) Open the browser
+	ChromeDriver driver = new ChromeDriver();
+	// 2) Maximize it
+	driver.manage().window().maximize();
+	// 3) Navigate to application 
+	driver.get("https://www.remove.bg/");
+	// 4) Click 'Select files to upload' button
+	driver.findElement(By.xpath("(//button [contains(@type, 'button')]) [3]")).click();
+	
+	Thread.sleep(3000);
+	// 5) Handover the control to AutoIT to select a file from the local machine, click on 'Open' button. 
+	Runtime.getRuntime().exec("resources//Tool.exe");
+	}
+
+}
